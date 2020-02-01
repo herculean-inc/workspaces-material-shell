86f5ec9cb0768f17ada48746fb4e54110547c362
Christian Lewis <ctjl96@gmail.com>
2020-02-01 09:27:46 -0600

    BULK AUTOMATIC COMMIT

--- workspaces-material-shell-1.0/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(workspaces-material-shell):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="workspaces-material-shell" command="/usr/bin/workspaces-material-shell"
--- workspaces-material-shell-1.0/debian/workspaces-material-shell.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the workspaces-material-shell package
-#
-0 4	* * *	root	[ -x /usr/bin/workspaces-material-shell_maintenance ] && /usr/bin/workspaces-material-shell_maintenance
